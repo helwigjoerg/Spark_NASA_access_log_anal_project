39eef5c8f068e622edb40cc03c48ee2c377df903
Joerg Helwig <helwigjoerg@gmail.com>
2018-08-28 12:22:23 +0000

    Revert "correct errors" Revert change
    This reverts commit 0f5215a20bbdd5133b8b61fee85d5d4c1303adea.
    
    commited by er

--- sparklogfilenasa/src/main/scala/helwig/joerg/sparklogfilenasa/CountingApp.scala
+++ sparklogfilenasa/src/main/scala/helwig/joerg/sparklogfilenasa/CountingApp.scala
@@ -7,31 +7,30 @@ import org.apache.spark.{SparkConf, SparkContext}
   * sbt "run inputFile.txt outputFile.txt"
   *  (+ select CountingLocalApp when prompted)
   */
-object LogFileAnalisisApp extends App{
-  val (inputFile) = (args(0))
+object CountingLocalApp extends App{
+  val (inputFile, outputFile) = (args(0), args(1))
   val conf = new SparkConf()
     .setMaster("local")
     .setAppName("my awesome app")
 
-  Runner.run(conf, inputFile)
+  Runner.run(conf, inputFile, outputFile)
 }
 
 /**
   * Use this when submitting the app to a cluster with spark-submit
   * */
-object LogFileAnalisisApp extends App{
-  val (inputFile) = (args(0))
+object CountingApp extends App{
+  val (inputFile, outputFile) = (args(0), args(1))
 
   // spark-submit command should supply all necessary config elements
-  Runner.run(new SparkConf(), inputFile)
+  Runner.run(new SparkConf(), inputFile, outputFile)
 }
 
 object Runner {
-  def run(conf: SparkConf, inputFile: String): Unit = {
+  def run(conf: SparkConf, inputFile: String, outputFile: String): Unit = {
     val sc = new SparkContext(conf)
     val rdd = sc.textFile(inputFile)
-    val counts = LogFileAnalisis.process (rdd)
+    val counts = WordCount.withStopWordsFiltered(rdd)
     counts.saveAsTextFile(outputFile)
   }
 }
-
